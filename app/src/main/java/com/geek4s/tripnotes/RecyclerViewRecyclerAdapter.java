5e5dfd51a28faf9bc6ee559890db1441d0422cbf
Murugawell <murugavel.cse1111@gmail.com>
2018-01-05 00:55:09 +0530

    ui fixes

--- app/src/main/java/com/geek4s/tripnotes/RecyclerViewRecyclerAdapter.java
+++ app/src/main/java/com/geek4s/tripnotes/RecyclerViewRecyclerAdapter.java
@@ -3,16 +3,18 @@ package com.geek4s.tripnotes;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
-import android.text.method.ScrollingMovementMethod;
 import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.geek4s.tripnotes.bean.People;
 import com.geek4s.tripnotes.bean.Trip;
@@ -20,6 +22,9 @@ import com.github.aakira.expandablelayout.ExpandableLayout;
 import com.github.aakira.expandablelayout.ExpandableLayoutListenerAdapter;
 import com.github.aakira.expandablelayout.ExpandableLinearLayout;
 import com.github.aakira.expandablelayout.Utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.util.List;
 
@@ -49,14 +54,55 @@ public class RecyclerViewRecyclerAdapter extends RecyclerView.Adapter<RecyclerVi
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         final People item = data.get(position);
+        int maxLength = 10;
         holder.setIsRecyclable(false);
-        holder.textView.setText(item.getName());
+
+        String displayPeopleName = formatedString(item.getName(), maxLength);
+        final String peopleName = item.getName();
+
+        holder.textView.setText(displayPeopleName);
+
+        holder.textView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(context, "This is " + peopleName, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
         holder.textviewamount.setText(item.getTotalAmountSpent() + "");
         holder.itemView.setBackgroundColor(ContextCompat.getColor(context, R.color.colorAccent));
         holder.expandableLayout.setInRecyclerView(true);
         holder.expandableLayout.setBackgroundColor(ContextCompat.getColor(context, R.color.background1));
         holder.expandableLayout.setInterpolator(Utils.createInterpolator(Utils.LINEAR_OUT_SLOW_IN_INTERPOLATOR));
         holder.expandableLayout.setExpanded(expandState.get(position));
+        JSONArray amt = item.getAmountSpentJSON();
+        if (amt != null) {
+            {
+                if (amt.length() > 0) {
+                    try {
+                        holder.layitem1.setVisibility(View.VISIBLE);
+                        String name = amt.getJSONObject(0).getString("for");
+                        String amount = amt.getJSONObject(0).getString("amount");
+                        holder.item1name.setText(formatedString(name, 10));
+                        holder.item1amount.setText(amount);
+                        if (amt.length() > 1) {
+                            holder.layitem2.setVisibility(View.VISIBLE);
+                            String name2 = amt.getJSONObject(1).getString("for");
+                            String amount2 = amt.getJSONObject(1).getString("amount");
+                            holder.item2name.setText(formatedString(name2, 10));
+                            holder.item2amount.setText(amount2);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (amt.length() < 3) {
+                    holder.showmore.setVisibility(View.GONE);
+                }
+            }
+        }
+
 
         holder.addNewSpent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -87,6 +133,19 @@ public class RecyclerViewRecyclerAdapter extends RecyclerView.Adapter<RecyclerVi
         });
     }
 
+    //    merthod to add padding at the eod of string
+    private String formatedString(String name, int maxLength) {
+        if (name.length() > maxLength) {
+            name = name.substring(0, maxLength) + "..";
+        }
+        if (name.length() < maxLength) {
+            int l = maxLength - name.length() + 2;
+            name = String.format("%-" + l + "s", name);
+        }
+        return name;
+
+    }
+
     private void onClickButton(final ExpandableLayout expandableLayout) {
         expandableLayout.toggle();
     }
@@ -97,7 +156,11 @@ public class RecyclerViewRecyclerAdapter extends RecyclerView.Adapter<RecyclerVi
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        public Button showmore;
         public TextView textView, listofPeolpleTextView, textviewamount;
+        public LinearLayout layitem1, layitem2;
+        public ImageView item1icon, item2icon;
+        public TextView item1name, item2name, item1amount, item2amount;
         public Button addNewSpent;
 
         public RelativeLayout buttonLayout;
@@ -110,17 +173,32 @@ public class RecyclerViewRecyclerAdapter extends RecyclerView.Adapter<RecyclerVi
         public ViewHolder(View v) {
             super(v);
             textView = (TextView) v.findViewById(R.id.textView);
+            item1name = (TextView) v.findViewById(R.id.people_item1_name);
+            item1amount = (TextView) v.findViewById(R.id.people_item1_amount);
+            item1icon = (ImageView) v.findViewById(R.id.people_item1_list_icon);
+            item2name = (TextView) v.findViewById(R.id.people_item2_name);
+            item2amount = (TextView) v.findViewById(R.id.people_item2_amount);
+            item2icon = (ImageView) v.findViewById(R.id.people_item2_list_icon);
             addNewSpent = (Button) v.findViewById(R.id.addnewspent_indiv_People);
+            showmore = (Button) v.findViewById(R.id.addnewspent_showmore_people);
             textviewamount = (TextView) v.findViewById(R.id.textView_amount);
             buttonLayout = (RelativeLayout) v.findViewById(R.id.button);
+            layitem1 = (LinearLayout) v.findViewById(R.id.layoutitem1);
+            layitem2 = (LinearLayout) v.findViewById(R.id.layoutitem2);
             expandableLayout = (ExpandableLinearLayout) v.findViewById(R.id.expandableLayout);
-            listofPeolpleTextView = (TextView) v.findViewById(R.id.people_list_textview);
+            layitem1.setVisibility(View.GONE);
+            layitem2.setVisibility(View.GONE);
+
+//            listofPeolpleTextView = (TextView) v.findViewById(R.id.people_list_textview);
 
 
 //            listofPeolpleTextView.setMovementMethod(new ScrollingMovementMethod());
 
         }
+
+
     }
+
 
     public ObjectAnimator createRotateAnimator(final View target, final float from, final float to) {
         ObjectAnimator animator = ObjectAnimator.ofFloat(target, "rotation", from, to);
